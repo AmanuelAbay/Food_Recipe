19a3f8a6cb4b6a4c898f568510a628694ef36d3a
AdoniasW <85018689+AdoniasW@users.noreply.github.com>
2022-01-14 04:25:36 -0800

    Update sign_up_users.vue

---
+++ src/components/pages/front/sign_up/sign_up_users.vue
@@ -0,0 +1,41 @@
+<template>
+<img class="logo" src="" />
+<h1>Sign Up</h1>
+<div class="register">
+    <input type="text" placeholder="Enter name"/>
+    <input type="email" placeholder="Enter email"/>
+    <input type="password" placeholder="Enter password"/>
+<button>Sign up</button>
+</div>
+<template>
+    <script>
+        export default {
+            name :"SignUp"
+
+</script>
+
+
+        }
+        <style>
+            .logo
+            {width:100%}
+            .register input {
+                width:300px;
+                height:40px;
+                padding-left:20px;
+                margin-bottom:30px;
+                margin-right:auto;
+                margin-left:auto;
+                border:1px solid skyblue;
+
+            }
+            .register button{
+                width:320px;
+                height:40px;
+                border:1px solid skyblue;
+                background:skyblue;
+                color:#fff;
+                cursor:pointer;
+            }
+        </style>
+    
